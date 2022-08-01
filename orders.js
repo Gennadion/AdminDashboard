016fe5c3471cf32191d108938ce57ceef664bdd4
Gennadion <47268114+Gennadion@users.noreply.github.com>
2022-08-01 13:13:27 +0300

    Main files

---
+++ orders.js
@@ -0,0 +1,56 @@
+const Orders = [
+    {
+        productName: 'Brand New SCRUM table',
+        productNumber: '34942',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Russ Hannemann "Tres Commas" Whiskey',
+        productNumber: '92643',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Hooli Box',
+        productNumber: '10045',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Pied Piper T-Shirt Medium',
+        productNumber: '94527',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Hooliphone Nucleus Beta',
+        productNumber: '28863',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ergonomic Gloves',
+        productNumber: '44296',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Cisco Nexus 3550 Series',
+        productNumber: '55009',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Kettle Salted Potato Crisps',
+        productNumber: '23287',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'GPS 4k Drone',
+        productNumber: '17162',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+]
